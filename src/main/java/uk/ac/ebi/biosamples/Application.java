a542e192bd5bae283d7cbd23ddce49378a85cbab
Kerruba <luca.cherubin@gmail.com>
2017-02-28 12:51:39 +0100

    Update imports

--- src/main/java/uk/ac/ebi/biosamples/Application.java
+++ src/main/java/uk/ac/ebi/biosamples/Application.java
@@ -1,6 +1,6 @@
 package uk.ac.ebi.biosamples;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import uk.ac.ebi.biosamples.Runners.FixedLengthRunner;
 
@@ -8,11 +8,7 @@ import uk.ac.ebi.biosamples.Runners.FixedLengthRunner;
 @SpringBootApplication
 public class Application {
 
-	@Autowired
-	FixedLengthRunner fixedLengthRunner;
-
 	public static void main(String[] args) {
-		Application.
 		SpringApplication.run(FixedLengthRunner.class);
 	}
 
